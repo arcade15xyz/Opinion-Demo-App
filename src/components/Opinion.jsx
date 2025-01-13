48ec8e3dd6c2e5f4e0d3e31c1e3e999e9b6ea64e
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-13 12:10:52 +0530

    adding Optimistic updating in opinion for value. we use 'useOptimistic' the argument for arrow function in useOptimistic namely prevVotes comes by react so no need to send it and mode by us. It is used so that we can use a temprory value for optimization untill the status is pending this value will render onlu until pending is true

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,14 +1,21 @@
-import { use ,useActionState } from "react";
+import { use, useActionState, useOptimistic } from "react";
 import { OpinionsContext } from "../store/opinions-context";
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
   const {upvoteOpinion, downvoteOpinion}=use(OpinionsContext);
+
+  cosnt [optimisticVotes,setVotesOptimistically] =useOptimistic(votes,
+     (prevVotes,mode)=> mode === 'up'?prevVotes + 1 : prevVotes - 1 
+    );
+
   async function upvoteAction(){
+    setVotesOptimistically('up');
     await upvoteOpinion(id);
     console.log('UPVOTE');
   }
 
   async function downvoteAction(){
+    setVotesOptimistically('down')
     console.log('DOWNVOTE');
     await downvoteOpinion(id);
   }
@@ -42,7 +49,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
         <button formAction={downvoteFormAction} disabled={upvotePending || downvotePending}>
           <svg
